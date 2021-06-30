bd9f9a909de232d2aaf3c5ab94a97ab5f32785cb
Sergio-DC <sgdc377@gmail.com>
2021-06-29 21:10:11 -0500

    grammar declaration implemented

--- mini-compiler/NodeType.py
+++ mini-compiler/NodeType.py
@@ -5,6 +5,7 @@ class NodeType(Enum):
 	ESC = 'ESC'
 	ARGS = 'ARGS'
 	LISTA_DECLARACIONES = 'LISTA_DECLARACIONES'
+	DECLARACION = 'DECLARACION'
 	PRINCIPAL = 'PRINCIPAL'
 	SCANF = 'SCANF' 
 	PRINTF = 'PRINTF'
@@ -15,9 +16,14 @@ class NodeType(Enum):
 	VARIABLE = 'VARIABLE'
 	COMMA = 'COMMA'
 
+	# Tipo de Dato
+	NUMBER_DATA_TYPE = 'number'
+	DOUBLE_DATA_TYPE = 'double'
+	TEXT_DATA_TYPE = 'text'
+
 if __name__ == '__main__':
-	print("Hola")
-	for type in NodeType:
-		if type.value == 'LPAREN':
-			print(type.name, type.value)
+	print("Hola: ", NodeType)
+	for type in NodeType.DATA_TYPE:
+		#if type.value == 'LPAREN':
+		print(type.name, type.value)
 
